4a3989dc47eedfdc7bc5ac18ec759d2b7463b766
HungLuongKT <lquochung97@gmail.com>
2021-09-01 10:14:55 +0700

    2nd commit push with pullme and pullme2

---
+++ git.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#git init
+#git status
+#git add .
+#git commit -m "some cmt"
+#git log
+#git remote add <name> <url>
+#git pull
+#git push -u <name> master
+#git branch anewbranch
+#git checkout anewbranch #switch to next branch
+
+#Step:
+#git --version
+#git config --global user.name "HungLuongKT"
+#git config --global user.email "you@example.com"
+
+#git init
+#git add pullme.txt
+
+#git status
+
+#git commit -m "1st commit pullme.txt"
+
+##git remote add origin https://github.com/HungLuongKT/test1.git
+##git push -u origin master
+
+#Pushing edited pullme.txt + pullme2.txt:
+git add .
